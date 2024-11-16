6c1e680c926f423801f432f28f176d501315e0c6
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2024-11-16 16:26:52 +0000

    understanding Functions
    trying to understanding Functions

---
+++ Function.py
@@ -0,0 +1,18 @@
+def area_of_circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    return radius
+
+Sword_length = 1.0
+
+Spear_length = 2.0
+
+Sword_area = area_of_circle(Sword_length)
+
+Spear_area = area_of_circle(Spear_length)
+
+print("Sword length:", Sword_length, "meters.")
+print("Sword attack area:", Spear_area, "square meters")
+
+print("Spear length:", Spear_length, "meters.")
+print("Spear attack area:", Spear_area, "square meters")
